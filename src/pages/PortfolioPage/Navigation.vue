44cbd809f9103cf7e2eb1c9fc05b0095c11f60f8
niek veenstra <niekveenstra14@gmail.com>
2025-04-29 23:49:47 +0200

    Nice gradient with custom underline specially crafted for my beloved text editor NeoVim.

--- src/pages/PortfolioPage/Navigation.vue
+++ src/pages/PortfolioPage/Navigation.vue
@@ -5,12 +5,11 @@ const {links} = defineProps({
 </script>
 
 <template>
-  <nav class="flex-1 flex text-sm flex-col justify-center items-start">
+  <nav class="flex-1 flex text-sm flex-col justify-center items-start ">
     <ul v-for="({name, isActive}) in links">
-      <li class="flex gap-4 items-center group">
+      <li class="flex gap-4 items-center group hover:cursor-pointer">
         <span :class="`link-seperator use-transition group-hover:bg-hero-text group-hover:w-16  ${isActive ? 'bg-hero-text w-16' : 'bg-web-text w-8 '}`"></span>
-        <span
-            :class="`peer link group-hover:text-hero-text ${isActive ? 'text-hero-text' : 'text-web-text'}`">
+        <span :class="`peer link group-hover:text-hero-text ${isActive ? 'text-hero-text' : 'text-web-text'}`">
               {{ name }}
             </span>
       </li>
--- src/pages/PortfolioPage/Profile.vue
+++ src/pages/PortfolioPage/Profile.vue
@@ -8,13 +8,23 @@
   </h1>
   <span id="hero-content" class="text-2xl italic pt-3 block ">
             Software Engineer
-          </span>
+  </span>
   <p class="text-web-text pt-4 text-base w-3/4">
     Passionate about embedded systems, web applications and mathematics. I use
-    <a class="underline underline-offset-2 hover-lighting" rel="noopener" target="_blank" href="https://neovim.io/"> NeoVim</a>, btw.
+    <span class="inline-flex group flex-col">
+      <a class="hover:transition-colors ease-in-out group-hover:text-transparent group-hover:bg-gradient-to-r
+          text-web-text gradient bg-clip-text"
+           rel="noopener" target="_blank" href="https://neovim.io/">NeoVim</a>
+      <span class="h-[0.02em] inline-block gradient group-hover:text-transparent bg-gradient-to-r "></span>
+      <span class="h-[0.02em] pt-[0.05em] transition bg-web-text gradient relative -top-1 w-full group-hover:bg-gradient-to-r"></span>
+    </span>, btw.
   </p>
 </template>
 
 <style scoped>
+@import 'tailwindcss';
 
+.gradient {
+  @apply from-blue-400 via-emerald-500 to-emerald-700
+}
 </style>
