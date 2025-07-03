9d2d7558502f701f3734947872b2d02dfe4d7386
Niek Veenstra <niekveenstra14@gmail.com>
2025-07-03 20:14:36 +0200

    Fixed the background lighting.

--- pages/PortfolioPage/MouseGradient.vue
+++ pages/PortfolioPage/MouseGradient.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 const coordinates = useMouseCoordinates();
 const mouseGradientBox = useTemplateRef('mouseGradientBox')
-onMounted(() => watchEffect(() => {
+const stylePosition = ref("background: radial-gradient(600px at 500px 500px, rgba(29, 78, 216, 0.15), transparent 80%);");
+ watchEffect(() => {
   const element = toValue(mouseGradientBox) as HTMLElement
-  element.style.top = coordinates.y.value - (element.offsetWidth / 2)  + "px";
-  element.style.left = coordinates.x.value - (element.offsetWidth / 2) + "px";
-}));
+  console.log("return?")
+  if (!element) return;
+  const x = coordinates.x.value  + "px";
+  const y = coordinates.y.value  + "px";
+  stylePosition.value = `background: radial-gradient(600px at ${x} ${y}, rgba(29, 78, 216, 0.15), transparent 80%);`
+});
 </script>
 
 <template>
-<div ref="mouseGradientBox" class="min-w-1/3 min-h-1/3 absolute rounded-4xl bg-red-500/40">
+<div ref="mouseGradientBox" :style="stylePosition" class="h-dvh pointer-events-none w-dvw fixed inset z-30 rounded-4xl">
 </div>
 </template>
 
