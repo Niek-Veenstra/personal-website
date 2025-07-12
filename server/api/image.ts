797f90a1105d0fbdd1fb27b5303bf9271a9e7804
Niek Veenstra <niekveenstra14@gmail.com>
2025-07-12 14:33:20 +0200

    Removed files.

--- server/api/image.ts
+++
@@ -1,9 +0,0 @@
-import { createIPX, createIPXH3Handler, ipxFSStorage } from "ipx";
-import { join } from "node:path";
-
-const ipx = createIPX({
-  storage: ipxFSStorage({
-    dir: join(process.cwd(), "images"),
-  }),
-});
-export default createIPXH3Handler(ipx);
