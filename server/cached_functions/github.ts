d627937f57641c5012023d9ea98df705be8df6b2
Niek Veenstra <niekveenstra14@gmail.com>
2025-07-14 17:10:05 +0200

    Redid types.

--- server/cached_functions/github.ts
+++ server/cached_functions/github.ts
@@ -1,9 +1,9 @@
 import type { GithubApiRepository } from "../types/github";
-import type { Repository } from "~/shared/types/github";
+import type { ProjectInformation } from "~/shared/types/github";
 import { status } from "http-status";
 import { writeImage } from "./../utils/image";
 import {
-  fetchOpenGraphImages,
+  fetchGithubUrl,
   getAuthenticatedUserInformation,
   getOpenGraphImageUrls,
   getOwnGithubProjects,
@@ -14,14 +14,14 @@ const HOUR_IN_SECONDS = 60 * 60;
 const addProjectPreview = async (
   repository: GithubApiRepository,
   owner: string,
-): Promise<Repository> => {
-  //@ts-ignore
+): Promise<string> => {
   let {
+    //@ts-ignore
     repository: { openGraphImageUrl },
   } = await getOpenGraphImageUrls(owner, repository.name);
-  const empty = { social_preview_url: "", ...repository };
+  const empty = "";
 
-  const openGraphImageResponse = await fetchOpenGraphImages(openGraphImageUrl);
+  const openGraphImageResponse = await fetchGithubUrl(openGraphImageUrl);
   if (openGraphImageResponse.status !== status.OK) return empty;
   const fileExtension =
     openGraphImageResponse.headers["content-type"]?.split("/")[1] ?? null;
@@ -32,36 +32,48 @@ const addProjectPreview = async (
     Buffer.from(openGraphImageResponse.data),
   );
 
-  return {
-    social_preview_url: `${repository.name}.${fileExtension}`,
-    ...repository,
-  };
+  return `${repository.name}.${fileExtension}`;
 };
 
-const fetchImplementation = async (): Promise<Array<Repository>> => {
+const fetchLanguages = async (
+  repository: GithubApiRepository,
+): Promise<Array<Record<string, number>>> => {
+  const response = await fetchGithubUrl(repository.languages_url);
+  return response.data;
+};
+
+const fetchImplementation = async (): Promise<Array<ProjectInformation>> => {
   const githubProjectsResponse = await getOwnGithubProjects();
   if (githubProjectsResponse.status !== status.OK) {
     return [];
   }
-  const projects = githubProjectsResponse.data;
+  const repositories = githubProjectsResponse.data;
   const authenticatedUserResponse = await getAuthenticatedUserInformation();
 
   if (authenticatedUserResponse.status !== status.OK) {
     return [];
   }
 
-  const promises: Array<Promise<Repository>> = [];
-  projects.forEach((project) => {
+  //Huge respect to the Haskell programming language for providing me this insight, i love Maybe.
+  const maybe = (promise: PromiseSettledResult<any>, nothingValue: any) =>
+    promise.status === "fulfilled" ? promise.value : nothingValue;
+  const promises: Array<Promise<ProjectInformation>> = [];
+  repositories.forEach((repository) => {
     promises.push(
-      addProjectPreview(project, authenticatedUserResponse.data.login),
+      Promise.allSettled([
+        addProjectPreview(repository, authenticatedUserResponse.data.login),
+        fetchLanguages(repository),
+      ]).then(([previewPromise, languagesPromise]) => ({
+        ...repository,
+        social_preview_url: maybe(previewPromise, ""),
+        languages: maybe(languagesPromise, []),
+      })),
     );
   });
 
   const rejectedHandler = () => [];
-  const resolveHandler = (promises: any[]) =>
-    promises
-      .filter((promise) => promise.status === "fulfilled")
-      .map((promise) => promise.value);
+  const resolveHandler = (promises: any[]): Array<ProjectInformation> =>
+    promises.map((promise) => promise.value);
   return await Promise.allSettled(promises).then(
     resolveHandler,
     rejectedHandler,
