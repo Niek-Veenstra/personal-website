39c3ce3f501e568466edc3d9b4f80589297b6661
Niek Veenstra <niekveenstra14@gmail.com>
2025-07-20 17:03:05 +0200

    Refix scrollspy

--- composables/scrollspy.ts
+++ composables/scrollspy.ts
@@ -10,17 +10,7 @@ export function useScrollSpy(
   return watch(windowScroll.y, () => {
     const sortF = (one: Section, two: Section) =>
       getBoundingBox(two).top - getBoundingBox(one).top;
-    const filterF = (section: Section) => getBoundingBox(section).top <= 0;
-
-    console.log(
-      sections.map((one) => {
-        console.log(one.element.value.id);
-
-        one.bb = getBoundingBox(one).top;
-        return one;
-      }),
-    );
-
+    const filterF = (section: Section) => getBoundingBox(section).top <= 200;
     const result = sections.filter(filterF).sort(sortF).at(0);
     activeSection.value = result ?? activeSection.value;
   });
--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@ const sectionClickHandler = (index: number) => {
     <div class="bg-slate-900">
       <MouseGradient></MouseGradient>
       <div class="min-h-[100dvh] flex flex-1 justify-center">
-        <div class="xl:flex justify-center max-w-7xl gap-4">
+        <div class="xl:flex flex-row justify-center max-w-7xl">
           <aside
             id="info"
             class="xl:h-1/2 flex-1 max-w-[40em] max-h-[100dvh] xl:pt-25 items-center pt-16 text-hero-text font-inter flex flex-col xl:justify-start xl:sticky xl:top-0"
@@ -49,20 +49,17 @@ const sectionClickHandler = (index: number) => {
             <div class="flex flex-1 xl:w-full flex-col justify-end">
               <ul
                 id="socials"
-                class="flex col-span-2 justify-center xl:justify-start justify-self-end mb-4 mt-4 xl:mb-20"
+                class="flex col-span-2 justify-center xl:justify-start justify-self-end pb-4 pt-4"
               >
                 <Socials :socials="socials"></Socials>
               </ul>
             </div>
           </aside>
-          <main
-            id="main-content"
-            class="xl:pt-25 max-w-xl flex flex-col break-words gap-20"
-          >
+          <main id="main-content" class="max-w-xl flex flex-col gap-20">
             <section
               ref="about"
               id="about"
-              class="text-base col-span-2 place-self-start font-inter w-full scroll-mt-32 justify-center items-center flex flex-col"
+              class="text-base col-span-2 place-self-start font-inter w-full justify-center items-center flex flex-col xl:pt-20"
             >
               <span class="w-3/4 xl:w-full">
                 <SideText></SideText>
@@ -81,7 +78,7 @@ const sectionClickHandler = (index: number) => {
                 </JobExperienceCard>
               </ul>
             </section>
-            <section ref="projects" id="projects" class="w-full">
+            <section ref="projects" id="projects">
               <ul :v-if="githubProjects.status.value === 'success'">
                 <ProjectCard
                   v-for="project in githubProjects.data?.value?.sort(
